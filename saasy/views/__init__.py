54416606d3ee312857826b3fbae92f4c30c0e0cd
Grant Viklund <grant.viklund@gmail.com>
2020-08-21 17:29:35 -0700

    Working out flow

--- saasy/views/__init__.py
+++ saasy/views/__init__.py
@@ -8,7 +8,7 @@ from .organization import OrganizationListView, OrganizationDetailView, Organiza
 from .project import ProjectListView, ProjectDetailView
 from .profile import ProfileDetailView
 
-from saasy.models import Organization
+from saasy.models import Organization, Project, Team
 
 class DashboardView(TemplateView):
     template_name = "saasy/dashboard.html"
@@ -16,5 +16,7 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         profile = self.request.user.saasy_profile.get(site=settings.SITE_ID)
-        context['organizations'] = Organization.on_site.filter(owner=profile)[:5]
+        context['organizations'] = Organization.on_site.filter(owner=profile)[:3]
+        context['projects'] = Project.objects.all()[:3]         # TODO: Filter on the current user's Organizations
+        context['teams'] = Team.objects.all()[:3]               # TODO: Filter on the current user Memberships
         return context
